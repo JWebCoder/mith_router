07f81a1fbab4903c5abf491fddde922c5a26deae
João Moura <joao.moura@b-i.com>
2020-06-16 02:11:37 +0100

    Improved mith version

--- example/deps.ts
+++ example/deps.ts
@@ -1,3 +1,3 @@
-export { Mith, Request, Response, NextFunction } from  'https://deno.land/x/mith@v0.8.0/mod.ts'
+export { Mith, Request, Response, NextFunction } from  'https://deno.land/x/mith@v0.8.1/mod.ts'
 export { Router } from '../mod.ts'
 export { resolve } from "https://deno.land/std@0.57.0/path/mod.ts"
